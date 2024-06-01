449270f64879d60fdbbf638d5fda7b8bad3cd0f8
mellina ezzahir <mellina.ezzahir@hotmail.com>
2024-06-01 20:53:39 +0100

    removed home button

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,7 +6,6 @@ function Navbar() {
   return (
     <div className="navbar">
       <h3>Mellina Ezzahir</h3>
-      <Link className="link navbarItem" to="/#">Home</Link>
       <Link className="link navbarItem" to="/">Certificates</Link>
       <Link className="link navbarItem" to="/">Education</Link>
       <Link className="link navbarItem" to="/projects">Projects</Link>
